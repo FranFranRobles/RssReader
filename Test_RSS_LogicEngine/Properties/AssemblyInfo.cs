0bb18b2b61967a51e173b9e39f95a0d08a6aa6de
bbusby <bbusby@eecs.wsu.edu>
2018-02-08 05:40:34 -0800

    Created a new logic engine project along with its corresponding test project. Clearly, the logic engine will contain all the application's logic while the UI will contain the user interface implementation. It is good to keep these separate.

---
+++ Test_RSS_LogicEngine/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test_RSS_LogicEngine")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test_RSS_LogicEngine")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4217c06f-39db-4dcf-b358-8529b62636b8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
