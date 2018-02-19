6dc79c282ea45b40f69532f9b3653ef5a93aa774
bbusby <bbusby@eecs.wsu.edu>
2018-02-18 21:41:13 -0800

    Map is now in aerial mode.

--- RSS_UI/MainWindow.xaml.cs
+++ RSS_UI/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Maps.MapControl.WPF;
 
 namespace RSS_UI
 {
@@ -23,6 +24,7 @@ namespace RSS_UI
         public MainWindow()
         {
             InitializeComponent();
+            myMap.Mode = new AerialMode(true);
         }
     }
 }
