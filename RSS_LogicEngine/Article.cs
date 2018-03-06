9530321186b70d04158fe90254241d4dd7886b18
francisco robles-fonseca <ffrancisc92@gmail.com>
2018-03-06 00:44:01 -0800

    article class and unit tests

---
+++ RSS_LogicEngine/Article.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSS_LogicEngine
+{
+    /*
+     * should  the class contain a keyword list?
+     * if so should the class contain a contains method?
+     */
+    public class Article
+    {
+        const string NOT_APPLICABLE = "N/A";
+        #region Article Atributes
+        
+        private string title; // name of the article
+        private string articleUrl; //location of full article
+        private string discription; // summary of the article
+        private string pubDate; // date published
+        #endregion
+        /// <summary>
+        /// Article object CTOR
+        /// </summary>
+        /// <param name="_title">name of the article</param>
+        /// <param name="_articleUrl">source of the complete article</param>
+        /// <param name="summary">short summary of the article</param>
+        /// <param name="_pubDate">date published</param>
+        /// <param name="_catagories">list of search keywords</param>
+        public Article(string _title, string _articleUrl, string summary = NOT_APPLICABLE, 
+            string _pubDate = NOT_APPLICABLE, List<string>_catagories = null)
+        {
+            title = _title;
+            articleUrl = _articleUrl;
+            discription = summary;
+            pubDate = _pubDate;
+        }
+        /// <summary>
+        /// function grabs the title of the article
+        /// </summary>
+        public string Title
+        {
+            get { return title; }
+        }
+        /// <summary>
+        /// function grabs the url of the full article
+        /// </summary>
+        public string ArticleUrl
+        {
+            get { return articleUrl; }
+        }
+        /// <summary>
+        /// function grabs the articles summary
+        /// </summary>
+        public string ArticleSummary
+        {
+            get { return discription; }
+
+        }
+        /// <summary>
+        /// function grabs the publication date fo the article
+        /// </summary>
+        public string PublicationDate
+        {
+            get { return pubDate; }
+        }
+    }
+}
