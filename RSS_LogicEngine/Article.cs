3c2e5ff6373f46c311aefe3d52e06b68adfb07f0
Douglas Yamamoto <douglas.yamamoto@wsu.edu>
2018-03-06 11:56:44 -0800

    Have some mechanisms and minor details figured out.  Mostly a proof of concept listed in the code

---
+++ RSS_LogicEngine/Article.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSS_LogicEngine
+{
+    public class Article
+    {
+        private string name;
+        private string date;
+
+        public Article(string name, string date) {
+            this.Name = name;
+            this.Date = date;
+        }       
+
+        // Standard getters/setters
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+    }
+}
--- RSS_UI/RSSWindow.xaml.cs
+++ RSS_UI/RSSWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using RSS_LogicEngine;
 
 namespace RSS_UI
 {
@@ -22,6 +23,36 @@ namespace RSS_UI
         public RSSWindow()
         {
             InitializeComponent();
+
+            var gridView = new GridView();
+            this.articleList.View = gridView;
+            gridView.Columns.Add(new GridViewColumn { Header = "Title", DisplayMemberBinding = new Binding("Title") });
+            gridView.Columns.Add(new GridViewColumn { Header = "Date", DisplayMemberBinding = new Binding("Date") });
+
+            webBrowser.Navigate("https://google.com");
+        }
+
+        private void buttonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            // Click event handler
+            // Should take the contents of the textbox, which is the RSS Feed url and add it to the tree view
+            // Should give the content to some creater to create the feed object appropriately
+
+            // Test for populating the articleList
+            //this.articleList.Items.Add(new Article("Test", "Dummy Date"));
+
+            // Test for populating the treeView
+            TreeViewItem newItem = new TreeViewItem();
+            newItem.Header = "Test Header";
+            this.treeView.Items.Add(newItem);
+        }
+
+        private void textBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                textBox.Text = "Doug rules";
+            }
         }
     }
 }
