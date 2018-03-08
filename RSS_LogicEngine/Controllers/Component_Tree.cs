774b2ec0d31deb7ee517b6daacf2d1c78f16adfd
bbusby <bbusby@eecs.wsu.edu>
2018-03-07 19:38:52 -0800

    Created incomplete Component_Tree

---
+++ RSS_LogicEngine/Controllers/Component_Tree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSS_LogicEngine.Controllers
+{
+    public class Component_Tree
+    {
+        private Component root;
+        public List<string> Get_Children_Of(string path)
+            => Get_Component_At(path).Get_Children();
+        public bool Is_Leaf(string path)
+            => Get_Component_At(path).Is_Leaf();
+        private Component Get_Component_At(string path)
+        {
+            Component curr = root;
+            string[] path_component = path.Split('/');
+            foreach (string s in path_component)
+            {
+                curr = curr.Get_Child(s);
+                if (curr == null)
+                    break;
+            }
+            return curr;
+        }
+    }
+}
---
+++ Test_RSS_LogicEngine/Test_Component_Tree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_RSS_LogicEngine
+{
+    [TestClass]
+    public class Test_Component_Tree
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
