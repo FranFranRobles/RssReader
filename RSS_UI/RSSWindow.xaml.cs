1493549c8bf72d3b6a3926d67a00aacf8d964d8f
Douglas Yamamoto <douglas.yamamoto@wsu.edu>
2018-03-08 22:47:05 -0800

    Implemented all, I think, mechanisms required for this window to be working for the Milestone.  Textboxes that are auto-populated with "Feed Name" and "RSS URL" will clear once the user clicks in the textbox to edit and fill values.  Clicking the button will create an item in the left hand TreeView menu.  Clicking the item in the TreeView will create an item in the Article list, which is the upper textbox.  Clicking the item in the Article list will change the webpage displayed at the bottom and will put some text in the box directly above the web browser.  The textbox directly above the web browser is mean to store the article summary.  All that is needed to complete the milestone is information that is coming back from the Logic Engine so that the UI can reflect all information properly!

--- RSS_UI/RSSWindow.xaml.cs
+++ RSS_UI/RSSWindow.xaml.cs
@@ -23,12 +23,21 @@ namespace RSS_UI
         {
             InitializeComponent();
 
+            // Format the list of articles that come from the feed selected in the tree menu
             var gridView = new GridView();
             this.articleList.View = gridView;
-            gridView.Columns.Add(new GridViewColumn { Header = "Date", DisplayMemberBinding = new Binding("Date") });
-            gridView.Columns.Add(new GridViewColumn { Header = "Title", DisplayMemberBinding = new Binding("Title") });
+            gridView.Columns.Add(new GridViewColumn { Header = "Date", DisplayMemberBinding = new Binding("Date"), Width = 100});
+            gridView.Columns.Add(new GridViewColumn { Header = "Title", DisplayMemberBinding = new Binding("Title"), Width = 483});     
+            articleList.SelectionChanged += articleList_SelectionChanged;  // Giving the articles an appropriate method to show in browser
 
             webBrowser.Navigate("https://google.com");
+        }
+
+        public class ArticleListItem
+        {
+            // Property definitions so that the articleList can bind to an ArticleListItem and organize properly
+            public String Title { get; set; }
+            public String Date { get; set; }
         }
 
         private void buttonAdd_Click(object sender, RoutedEventArgs e)
@@ -40,39 +49,38 @@ namespace RSS_UI
             this.treeView.Items.Add(newItem);   
         }
 
-        private void urlBox_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-                urlBox.Text = "Doug rules";
-            }
-        }
-
-        private void urlBox_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                urlBox.Text = "";
-            }
-            else
-                urlBox.Text = "False";
-        }
 
         private void component_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             ArticleListItem newArticle = new ArticleListItem();
             newArticle.Title = "test";
             newArticle.Date = "test date";
-            
+
             articleList.Items.Add(newArticle);
         }
 
-        public class ArticleListItem
+        private void articleList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            public String Title { get; set; }
-            public String Date { get; set; }
+            FlowDocument newContent = new FlowDocument();
+            Paragraph newP = new Paragraph();
+            Run newRun = new Run("EE451 was selected, RichTextBox is kind of weird!");
+            newP.Inlines.Add(newRun);
+            newContent.Blocks.Add(newP);
 
+            webBrowser.Navigate("http://www.eecs.wsu.edu/~fischer/ee451year2018.html");
+            summaryBox.Document = newContent;
+        }
 
+        private void nameBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (nameBox.Text == "Feed Name")
+                nameBox.Clear();
+        }
+
+        private void urlBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (urlBox.Text == "RSS URL")
+                urlBox.Clear();
         }
     }
 }
