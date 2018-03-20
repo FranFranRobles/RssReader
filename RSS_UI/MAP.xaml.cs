5c7b16efeb1d2fdf2fdc6f9d87fe34ef5bcefbcd
Timothy Kaiser <timothy.kaiser@wsu.edu>
2018-03-20 12:42:38 -0700

    redesigned ui to utilize a single window with loadable content

---
+++ RSS_UI/MAP.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Maps.MapControl.WPF;
+
+
+namespace RSS_UI
+{
+    /// <summary>
+    /// Interaction logic for MAP.xaml
+    /// </summary>
+    public partial class MAP : UserControl
+    {
+        public MAP()
+        {
+            InitializeComponent();
+            myMap.Focus();
+            myMap.Mode = new AerialMode(true);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Content = new RSS();
+        }
+    }
+}
+
