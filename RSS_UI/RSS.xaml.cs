aa75c608f527ddd0db50b85f3129179ff35c2132
Timothy Kaiser <timothy.kaiser@wsu.edu>
2018-03-23 15:09:16 -0700

    Map Properly Implemented. RSS Resize implemented. Need to adjust RSS element sizes (specifically text sizes)

--- RSS_UI/RSS.xaml.cs
+++ RSS_UI/RSS.xaml.cs
@@ -64,6 +64,7 @@ namespace RSS_UI
         {
             string rssURL = urlBox.Text;                                    // Get names listed in the textboxes
             string feedName = nameBox.Text;
+            nameBox.FontSize = 2;
 
             ComponentTreeViewItem newFeed = new ComponentTreeViewItem();    // Create item to be displayed in left hand menu
             newFeed.Header = feedName;                                      // Reflect the name in the menu properly 
@@ -160,47 +161,14 @@ namespace RSS_UI
             this.Content = new MAP();
 
         }
+
+        private void nameBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
     }
 }
 
 
 
-
-
-//public partial class RSS : UserControl
-//{
-//    public RSS()
-//    {
-//        InitializeComponent();
-
-//        var gridView = new GridView();
-//        this.articleList.View = gridView;
-//        gridView.Columns.Add(new GridViewColumn { Header = "Title", DisplayMemberBinding = new Binding("Title") });
-//        gridView.Columns.Add(new GridViewColumn { Header = "Date", DisplayMemberBinding = new Binding("Date") });
-
-//        webBrowser.Navigate("https://google.com");
-//    }
-
-//    private void buttonAdd_Click(object sender, RoutedEventArgs e)
-//    {
-//        // Click event handler
-//        // Should take the contents of the textbox, which is the RSS Feed url and add it to the tree view
-//        // Should give the content to some creater to create the feed object appropriately
-
-//        // Test for populating the articleList
-//        //this.articleList.Items.Add(new Article("Test", "Dummy Date"));
-
-//        // Test for populating the treeView
-//        TreeViewItem newItem = new TreeViewItem();
-//        newItem.Header = "Test Header";
-//        this.treeView.Items.Add(newItem);
-//    }
-
-//    private void textBox_KeyDown(object sender, KeyEventArgs e)
-//    {
-//        if (e.Key == Key.Enter)
-//        {
-//            textBox.Text = "Doug rules";
-//        }
-//    }
 
