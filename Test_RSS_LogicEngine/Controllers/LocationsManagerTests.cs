9b9d55d3fc15a402588a97ff74b71882d433a30e
francisco robles-fonseca <ffrancisc92@gmail.com>
2018-04-04 11:49:29 -0700

    update from staging plus commented out locations manager

---
+++ Test_RSS_LogicEngine/Controllers/LocationsManagerTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RSS_LogicEngine.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSS_LogicEngine.Controllers.Tests
+{
+    [TestClass()]
+    public class LocationsManagerTests
+    {
+        //const string LOCATIONS_MGR_TESTS = "Locations Manager Tests";
+
+
+        //[TestMethod()]
+        //[TestCategory(LOCATIONS_MGR_TESTS)]
+        //public void GetInstanceTest()
+        //{
+        //    Assert.IsNotNull(LocationsManager.GetInstance(), "locations Manager Object is set to a null reference");
+        //}
+
+        //[TestMethod()]
+        //[TestCategory(LOCATIONS_MGR_TESTS)]
+        //public void HasCordinatesTest()
+        //{
+        //    Assert.Fail();
+        //}
+        //[TestMethod()]
+        //[TestCategory(LOCATIONS_MGR_TESTS)]
+        //public void NoCordinatesTest()
+        //{
+        //    Assert.Fail();
+        //}
+    }
+}
