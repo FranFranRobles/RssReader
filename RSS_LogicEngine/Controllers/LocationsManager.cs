7c4587db574403276d36144e82944870dc14c6fa
francisco robles-fonseca <ffrancisc92@gmail.com>
2018-04-07 20:07:12 -0700

    Locations Manager Unit Tests

--- RSS_LogicEngine/Controllers/LocationsManager.cs
+++ RSS_LogicEngine/Controllers/LocationsManager.cs
@@ -8,55 +8,60 @@ namespace RSS_LogicEngine.Controllers
 {
     public class LocationsManager
     {
-    //    private LocationsManager instance = null;
-    //    private Dictionary<string, Tuple<bool, string, string>> locationsDictionary;
-    //    private LocationsManager() { }
-    //    public static LocationsManager GetInstance()
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-    //    public Tuple<string, string> GetCordinates(string text)
-    //    {
-    //        string[] words = Parse(text);
-    //        int index = 0;
-    //        bool found = false;
-    //        Tuple<bool, string, string> temp = null;
-    //        while (found == false && index < words.Length)
-    //        {
-    //            temp = SearchDictionary(words[index]);
-    //            found = IsValid(temp);
-    //            if (found == false)
-    //            {
-    //                temp = SearchDictionary(words[index]);
-    //                found = temp.Item1;
-    //                AddToDictionary(words[index], temp);
-    //            }
-    //            index++;
-    //        }
-    //        return new Tuple<string, string>(temp.Item2, temp.Item3);
+        private LocationsManager instance = null;
+        private Dictionary<string, Tuple<bool, string, string>> locationsDictionary;
+        private string latidude = "";
+        private string longitude = "";
+        private LocationsManager() { }
+        public static LocationsManager GetInstance()
+        {
+            throw new NotImplementedException();
+        }
+        public string Latidude() => latidude;
+        public string Longitude() => longitude;
+        public bool HasCordinates(string text)
+        {
+            string[] words = Parse(text);
+            int index = 0;
+            bool found = false;
+            Tuple<bool, string, string> temp = null;
+            bool hasCordinates = false; // need to add code for it to work
+            while (found == false && index < words.Length)
+            {
+                temp = SearchDictionary(words[index]);
+                found = IsValid(temp);
+                if (found == false)
+                {
+                    temp = SearchDictionary(words[index]);
+                    found = temp.Item1;
+                    AddToDictionary(words[index], temp);
+                }
+                index++;
+            }
+            return hasCordinates;
 
-    //        throw new NotImplementedException();
-    //    }
-    //    private string[] Parse(string Text)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-    //    private Tuple<bool, string, string> SearchDictionary(string city)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-    //    private bool IsValid(Tuple<bool, string, string> city)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-    //    private void AddToDictionary(string city, Tuple<bool, string, string> details)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-    //    private Tuple<bool, string, string> SearchForCity(string word)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
+            throw new NotImplementedException();
+        }
+        private string[] Parse(string Text)
+        {
+            throw new NotImplementedException();
+        }
+        private Tuple<bool, string, string> SearchDictionary(string city)
+        {
+            throw new NotImplementedException();
+        }
+        private bool IsValid(Tuple<bool, string, string> city)
+        {
+            throw new NotImplementedException();
+        }
+        private void AddToDictionary(string city, Tuple<bool, string, string> details)
+        {
+            throw new NotImplementedException();
+        }
+        private Tuple<bool, string, string> SearchForCity(string word)
+        {
+            throw new NotImplementedException();
+        }
 
     }
 }
--- Test_RSS_LogicEngine/Controllers/LocationsManagerTests.cs
+++ Test_RSS_LogicEngine/Controllers/LocationsManagerTests.cs
@@ -11,27 +11,42 @@ namespace RSS_LogicEngine.Controllers.Tests
     [TestClass()]
     public class LocationsManagerTests
     {
-        //const string LOCATIONS_MGR_TESTS = "Locations Manager Tests";
+        const string LOCATIONS_MGR_TESTS = "Locations Manager Tests";
+        const string INVALID_CORDINATE = "";
 
 
-        //[TestMethod()]
-        //[TestCategory(LOCATIONS_MGR_TESTS)]
-        //public void GetInstanceTest()
-        //{
-        //    Assert.IsNotNull(LocationsManager.GetInstance(), "locations Manager Object is set to a null reference");
-        //}
+        [TestMethod()]
+        [TestCategory(LOCATIONS_MGR_TESTS)]
+        public void GetInstanceTest()
+        {
+            Assert.IsNotNull(LocationsManager.GetInstance(), "locations Manager Object is set to a null reference");
+        }
 
-        //[TestMethod()]
-        //[TestCategory(LOCATIONS_MGR_TESTS)]
-        //public void HasCordinatesTest()
-        //{
-        //    Assert.Fail();
-        //}
-        //[TestMethod()]
-        //[TestCategory(LOCATIONS_MGR_TESTS)]
-        //public void NoCordinatesTest()
-        //{
-        //    Assert.Fail();
-        //}
+        [TestMethod()]
+        [TestCategory(LOCATIONS_MGR_TESTS)]
+        public void HasCordinatesTest()
+        {
+            string[] validCities = { "everett", "freeday seattle", "in homes in miami", "lost in MiAmI" };
+            LocationsManager myLocations = LocationsManager.GetInstance();
+            foreach (string city in validCities)
+            {
+                Assert.IsTrue(myLocations.HasCordinates(city));
+                Assert.AreNotEqual(INVALID_CORDINATE, myLocations.Latidude(), "search should return a latidude cordinate");
+                Assert.AreNotEqual(INVALID_CORDINATE, myLocations.Longitude(), "search should return a longitude cordinate");
+            }
+        }
+        [TestMethod()]
+        [TestCategory(LOCATIONS_MGR_TESTS)]
+        public void NoCordinatesTest()
+        {
+            string[] notACity = { "blah", "sjsj blah in", "the in F a"};
+            LocationsManager myLocations = LocationsManager.GetInstance();
+            foreach (string text in notACity)
+            {
+                Assert.IsFalse(myLocations.HasCordinates(text));
+                Assert.AreEqual(INVALID_CORDINATE, myLocations.Latidude(), "search should not return a latidude cordinate");
+                Assert.AreEqual(INVALID_CORDINATE, myLocations.Longitude(), "search should not return a longitude cordinate");
+            }
+        }
     }
 }
